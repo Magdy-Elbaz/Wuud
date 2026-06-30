5bc37918054945283cf28d219b507c3acb56f27f
MagdyElbaz <magdymaamoon17@gmail.com>
2026-06-30 20:53:08 +0300

    update

--- src/Components/Website/Home/Products/MetaProduct.js
+++ src/Components/Website/Home/Products/MetaProduct.js
@@ -1,16 +1,19 @@
 import ShowProductsQuantity from "../../ShowproductsQuantity";
 import Stars from "../../CustomerComments/Stars";
+import { useTranslation } from "react-i18next";
 
 export default function MetaProduct({ product, stock, singleProduct }) {
   const sale = product.price - product.discount;
+  const { t } = useTranslation();
+
   return (
     <div data-aos={singleProduct && "fade-up"}>
       <ShowProductsQuantity stock={stock} />
       <Stars product={product} />
       <div className="d-flex align-items-center gap-1">
-        <p className="m-0 text-primary fs-5 fw-bold">${sale}</p>
+        <p className="m-0 text-primary fs-5 fw-bold">{t('EGP')}{sale}</p>
         {sale !== +product.price && (
-          <span className="text-decoration-line-through">${product.price}</span>
+          <span className="text-decoration-line-through">{t('EGP')}{product.price}</span>
         )}
       </div>
     </div>
